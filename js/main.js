44e177929c5dccd8928ab04adf843e84ff5a7572
Pier Giulio Minetti <pg.minetti@gmail.com>
2020-11-24 16:10:19 +0100

    Filter Working

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 var app = new Vue({
     el: '#app',
     data: {
-      album : []
+      album : [],
+      actualGenre : 'all',
     },
     /**
      * 
@@ -17,11 +18,29 @@ var app = new Vue({
         // handle error
         console.log(error);
       })
-    },
+    }, //-->end created
     /**
      * 
      */
     methods: {
+      refresh(){
+        const self = this;
 
-    }
-  })
+      axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then(function (result) {
+        let cdsList = result.data.response;
+        
+        if(self.actualGenre !== 'all') {
+          cdsList = cdsList.filter( cd => cd.genre.toLowerCase() === self.actualGenre)
+        }  
+
+        self.album = cdsList;
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      }
+    } //--> end methods
+
+  }) //--> end vue js
